fb77a338e96b2c738d646308fd9e89790709535f
Aris Anggara Putra <arisanggara72@gmail.com>
2022-12-19 13:17:30 +0700

    mengubah test zalora

--- test_zalora.py
+++ test_zalora.py
@@ -7,18 +7,12 @@ import time
 
 driver = uc.Chrome()
 def test_Zalora():
-    driver.get("https://www.zalora.co.id/")
-    driver.maximize_window()
-    assert "ZALORA Indonesia: Belanja Online" in driver.title
-    driver.implicitly_wait(10)
-    
-    # account = driver.find_element(By.ID, "account__icon")
-
-    # Hover = ActionChains(driver).move_to_element(account)
-    # Hover.perform()
-
-    ActionChains(driver).move_to_element((driver.find_element(By.ID, "account__icon"))).perform()
-    driver.find_element(By.ID, "account-menu-login-btn").click()
+    driver.get("https://www.zalora.co.id/") #membuka halaman zalora
+    driver.maximize_window() #memperbesar windows
+    assert "ZALORA Indonesia: Belanja Online" in driver.title #mengecek title dari halaman zalora
+    driver.implicitly_wait(10) #menunggu proses selama 10 detik
+    ActionChains(driver).move_to_element((driver.find_element(By.ID, "account__icon"))).perform() #membuka hover menu
+    driver.find_element(By.ID, "account-menu-login-btn").click() #meng-klik 
     driver.find_element(By.NAME, "LoginForm[email]").send_keys("arisanggara310@gmail.com")
     driver.find_element(By.NAME, "LoginForm[password]").send_keys("Traine123")
     driver.find_element(By.CSS_SELECTOR, "button[class='btn btn--fluid fsxl btn-dark login__button']").click()
